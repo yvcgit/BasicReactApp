0db953f1a9168ef472051c4afa23655d868a3024
siva9900 <sivamanichavvakula@gmail.com>
2023-06-12 21:31:41 +0530

    project

--- src/components/forms/updateProjectDetails.js
+++ src/components/forms/updateProjectDetails.js
@@ -6,6 +6,38 @@ import SelectField from "../inputs/selectField";
 import TextInput from "../inputs/textInput";
 
 export default function UpdateProject() {
+  const [Projects, setProjects] = React.useState([]);
+  const [ProjectsUpdated, setProjectsUpdated] = React.useState({
+    id: "",
+    ProjectName: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://172.17.160.1:2023/getAllProjects")
+      .then((response) => {
+        setProjects(response.data || []);
+      })
+      .catch((error) => console.log(error));
+  }, [""]);
+
+  const updateProject = async () => {
+    await axios
+      .post("http://172.17.160.1:2023/updateProject", {
+        ...ProjectsUpdated,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    setProjectsUpdated({
+      id: "",
+      ProjectName: "",
+    });
+  };
   return (
     <Box p={2} m={2}>
       <Grid container spacing={2} justifyContent="center">
@@ -20,23 +52,57 @@ export default function UpdateProject() {
                 </Grid>
 
                 <Grid item md={12}>
-                  <SelectField label="Select Project" />
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      {"Select Project"}
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={ProjectsUpdated.id}
+                      onChange={(e) => {
+                        setProjectsUpdated({
+                          ...ProjectsUpdated,
+                          id: e.target.value,
+                        });
+                      }}
+                      label="Age"
+                    >
+                      {Projects.map((item, index) => {
+                        return (
+                          <MenuItem value={item.id}>
+                            {item.ProjectName}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </FormControl>
                 </Grid>
 
                 <Grid item md={12}>
-                  <TextInput
+                  <TextField
                     fullWidth
                     label="Project Name"
                     variant="outlined"
                     type="text"
+                    onChange={(e) => {
+                      setProjectsUpdated({
+                        ...ProjectsUpdated,
+                        ProjectName: e.target.value,
+                      });
+                    }}
                   />
                 </Grid>
                 <Grid item md={12}>
-                  <InputButton
-                    label="Update Project"
+                  <Button
+                    onClick={() => {
+                      updateProject();
+                    }}
                     variant="contained"
                     color="primary"
-                  />
+                  >
+                    Update Project
+                  </Button>
                 </Grid>
               </Grid>
             </Box>
