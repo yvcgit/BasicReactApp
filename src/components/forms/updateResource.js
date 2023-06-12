9ba5a4d1210bda0528cdef446f2437e50b4d27c4
siva9900 <sivamanichavvakula@gmail.com>
2023-06-12 21:27:47 +0530

    update resounce

--- src/components/forms/updateResource.js
+++ src/components/forms/updateResource.js
@@ -1,13 +1,21 @@
-import { Grid, Paper, Typography } from "@mui/material";
+import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect } from "react";
 import InputButton from "../inputs/inputButton";
 import SelectField from "../inputs/selectField";
 import TextInput from "../inputs/textInput";
 import axios from "axios";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function UpdateResource() {
   const [Resources, setResources] = React.useState([]);
+  const [ResourcesUpdated, setResourcesUpdated] = React.useState({
+    id: "",
+    resourceName: "",
+  });
 
   useEffect(() => {
     axios
@@ -18,18 +26,23 @@ export default function UpdateResource() {
       .catch((error) => console.log(error));
   }, [""]);
 
-  [
-    {
-        "id": 0,
-        "resourceId": "vy57431",
-        "resourceName": "Venkata"
-    },
-    {
-        "id": 0,
-        "resourceId": "nmn",
-        "resourceName": "mnbhj"
-    }
-]
+  const updateResource = async () => {
+    await axios
+      .post("http://172.17.160.1:2023/updateResource", {
+        ...ResourcesUpdated,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    setResourcesUpdated({
+      id: "",
+      resourceName: "",
+    });
+  };
 
   return (
     <Box p={2} m={2}>
@@ -45,23 +58,57 @@ export default function UpdateResource() {
                 </Grid>
 
                 <Grid item md={12}>
-                  
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      {"Select Resource"}
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={ResourcesUpdated.id}
+                      onChange={(e) => {
+                        setResourcesUpdated({
+                          ...ResourcesUpdated,
+                          id: e.target.value,
+                        });
+                      }}
+                      label="Age"
+                    >
+                      {Resources.map((item, index) => {
+                        return (
+                          <MenuItem value={item.id}>
+                            {item.resourceName}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </FormControl>
                 </Grid>
 
                 <Grid item md={12}>
-                  <TextInput
+                  <TextField
                     fullWidth
                     label="Resource Name"
                     variant="outlined"
                     type="text"
+                    onChange={(e) => {
+                      setResourcesUpdated({
+                        ...ResourcesUpdated,
+                        resourceName: e.target.value,
+                      });
+                    }}
                   />
                 </Grid>
                 <Grid item md={12}>
-                  <InputButton
-                    label="Update Resource"
+                  <Button
+                    onClick={() => {
+                      updateResource();
+                    }}
                     variant="contained"
                     color="primary"
-                  />
+                  >
+                    Update Resource
+                  </Button>
                 </Grid>
               </Grid>
             </Box>
